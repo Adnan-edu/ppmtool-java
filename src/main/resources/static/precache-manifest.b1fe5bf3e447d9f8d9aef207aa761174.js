87ba4035e0613c15120eab775e3c8e972f992e7b
mdashikadnan <md.ashikadnan@gmail.com>
2022-07-30 17:05:39 +1000

    added react

---
+++ src/main/resources/static/precache-manifest.b1fe5bf3e447d9f8d9aef207aa761174.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e55b0efca7f03746741452d88acd66d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39ee9d94a6e5792d8c0d",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "17baa8a9d00f0dabfa5f",
+    "url": "/static/css/main.d3c1742d.chunk.css"
+  },
+  {
+    "revision": "39ee9d94a6e5792d8c0d",
+    "url": "/static/js/2.46708143.chunk.js"
+  },
+  {
+    "revision": "17baa8a9d00f0dabfa5f",
+    "url": "/static/js/main.048f6c15.chunk.js"
+  },
+  {
+    "revision": "a58a2d5e2e2bf522cd66",
+    "url": "/static/js/runtime-main.c2ff858f.js"
+  }
+]);
